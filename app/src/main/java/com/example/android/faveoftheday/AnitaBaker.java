e56f8f6949ece14b69b9f08d98900f50ede502a2
CJ Middleton <c_middleton@hotmail.com>
2016-08-10 18:35:41 -0500

    Structure and Intent Comments

--- app/src/main/java/com/example/android/faveoftheday/AnitaBaker.java
+++ app/src/main/java/com/example/android/faveoftheday/AnitaBaker.java
@@ -10,21 +10,5 @@ public class AnitaBaker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anita_baker);
     }
-    /*
-    // Calls 'The Songstress' album screen
 
-        TheSongstress.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent TheSongstressIntent = new Intent(AnitaBakerActivity.this, TheSongstress.class);
-                startActivity(TheSongstressIntent);
-            }
-        });
-            //Will use MediaPlayer class to start music with click of album button
-
-            // Will use and ArrayList for storing music tracks for 'The Songstress'
-             private ArrayList<Track> tracks;
-
-     */
 }
